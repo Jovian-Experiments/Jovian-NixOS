fff0c433932ef84c1888d331aa7ce427b6b750a9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-23 19:20:00 -0400

    default.nix: Init
    This provides helpful output to build the ISOs. Though I don't think the
    ISOs here are strictly needed for an install, as the most recent kernel
    might be just fine for most purposes.
    
    This also provides access to the overlaid Nixpkgs with the `pkgs`
    attribute.

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import ./nixpkgs.nix {} }:
+
+let
+  nixpkgsPath = pkgs.path;
+  fromPkgs = path: pkgs.path + "/${path}";
+  evalConfig = import (fromPkgs "nixos/lib/eval-config.nix");
+  buildConfig = { configuration ? {} }:
+    evalConfig {
+      specialArgs = {
+        inherit nixpkgsPath;
+      };
+      modules= [
+          ./modules
+          configuration
+      ];
+    }
+  ;
+  eval = buildConfig { };
+in
+{
+  isoMinimal = (buildConfig {
+    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-minimal.nix");
+  }).config.system.build.isoImage;
+  isoGnome = (buildConfig {
+    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix");
+  }).config.system.build.isoImage;
+  isoPlasma = (buildConfig {
+    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix");
+  }).config.system.build.isoImage;
+  inherit (eval) pkgs;
+}
