2eedfe898043029b3afbbb7f020c4563a2cab706
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-22 22:04:54 -0400

    linux_jovian: init at 5.13.0-valve10.3

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,18 @@
 final: super:
 
+let
+  inherit (final)
+    kernelPatches
+    linuxPackagesFor
+  ;
+in
 {
+  linuxPackages_jovian = linuxPackagesFor final.linux_jovian;
+  linux_jovian = super.callPackage ./pkgs/linux-jovian {
+    kernelPatches = [
+      kernelPatches.bridge_stp_helper
+      kernelPatches.request_key_helper
+      kernelPatches.export-rt-sched-migrate
+    ];
+  };
 }
---
+++ pkgs/linux-jovian/default.nix
@@ -0,0 +1,71 @@
+{ lib, fetchFromGitHub, buildLinux, ... } @ args:
+
+let
+  inherit (lib)
+    concatStringsSep
+    splitVersion
+    take
+    versions
+  ;
+
+  kernelVersion = "5.13.0";
+  vendorVersion = "valve10.3";
+in
+buildLinux (args // rec {
+  version = "${kernelVersion}-${vendorVersion}";
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = versions.majorMinor version;
+
+  structuredExtraConfig = with lib.kernel; {
+    #
+    # From the downstream packaging
+    # -----------------------------
+    #
+
+    ##
+    ## Neptune stuff
+    ##
+
+    # Doesn't build on latest tag, not used in neptune hardware (?)
+    SND_SOC_CS35L36 = no;
+    # Update this to  = yes to workaround initialization issues and deadlocks when loaded as module;
+    # The cs35l41 / acp5x drivers in EV2 fail IRQ initialization with this set to  = yes, changed back
+    SPI_AMD = module;
+
+    # Works around issues with the touchscreen driver
+    PINCTRL_AMD = yes;
+
+    JUPITER = module;
+    SND_SOC_CS35L41 = module;
+    SND_SOC_CS35L41_SPI = module;
+
+    SND_SOC_AMD_ACP5x = module;
+    SND_SOC_AMD_VANGOGH_MACH = module;
+    SND_SOC_WM_ADSP = module;
+    SND_SOC_CS35L41_I2C = no;
+    SND_SOC_NAU8821 = module;
+    # Enabling our ALS, only in jupiter branches at the moment
+    LTRF216A = module;
+
+    # PARAVIRT options have overhead, even on bare metal boots. They can cause
+    # spinlocks to not be inlined as well. Either way, we don't intend to run this
+    # kernel as a guest, so this also clears out a whole bunch of
+    # virtualization-specific drivers.
+    HYPERVISOR_GUEST = lib.mkForce no;
+
+    #
+    # Fallout from the vendor-set options
+    # -----------------------------------
+    #
+    KVM_GUEST = lib.mkForce (option no);
+    MOUSE_PS2_VMMOUSE = lib.mkForce (option no);
+  };
+
+  src = fetchFromGitHub {
+    owner = "Jovian-Experiments";
+    repo = "linux";
+    rev = version;
+    hash = "sha256-Jj3Rj1hNHy9tXzDUzLmmkIN8Aodm7QMAGQlDcgvI/ig=";
+  };
+} // (args.argsOverride or { }))
