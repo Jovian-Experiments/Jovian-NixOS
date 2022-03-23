92f3782dc060374df18d017bbf7a44c0cdc7ec74
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-23 17:49:15 -0400

    modules: Add hw-support for misc things
    These are things done in the hw-support downstream package.

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./graphical.nix
+    ./hw-support.nix
     ./kernel.nix
     ./overlay.nix
     ./sound.nix
---
+++ modules/hw-support.nix
@@ -0,0 +1,51 @@
+{ config, lib, ... }:
+
+# Misc. settings set-up by the hw-support package
+
+let
+  inherit (lib)
+    mkDefault
+    mkIf
+    mkMerge
+    mkOption
+    types
+  ;
+  cfg = config.jovian;
+in
+{
+  options = {
+    jovian = {
+      enableDefaultSysctlConfig = mkOption {
+        default = true;
+        type = types.bool;
+      };
+      enableDefaultCmdlineConfig = mkOption {
+        default = true;
+        type = types.bool;
+      };
+    };
+  };
+
+  config = mkMerge [
+    (mkIf (cfg.enableDefaultCmdlineConfig) {
+      boot.kernelParams = [
+        # From grub-steamos
+        "amd_iommu=off"
+        "amdgpu.gttsize=8128"
+        "spi_amd.speed_dev=1"
+        "audit=0"
+      ];
+    })
+    (mkIf (cfg.enableDefaultSysctlConfig) {
+      boot.kernel.sysctl = {
+        # From 99-sysctl.conf
+        "kernel.sched_cfs_bandwidth_slice_u" = mkDefault 3000;
+        "kernel.sched_latency_ns" = mkDefault 3000000;
+        "kernel.sched_min_granularity_ns" = mkDefault 300000;
+        "kernel.sched_wakeup_granularity_ns" = mkDefault 500000;
+        "kernel.sched_migration_cost_ns" = mkDefault 50000;
+        "kernel.sched_nr_migrate" = mkDefault 128;
+      };
+    })
+  ];
+}
