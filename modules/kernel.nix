8539022617c51800b2cf6f45fa9b7b69b530c851
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-04-09 15:41:11 -0400

    kernel: Add DRM orientation quirk for Valve Steam Deck

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -1,5 +1,52 @@
 { config, lib, pkgs, ... }:
 
+let
+  inherit (lib)
+    mkDefault
+    mkIf
+    mkMerge
+    mkOption
+    types
+  ;
+
+  kernelBranch = config.boot.kernelPackages.kernel.meta.branch;
+
+  # FIXME: Stop hardcoding patch to 5.13
+  # TODO: Make an attrset of kernel major versions with the appropriate patches.
+  hasKernelPatches = kernelBranch == "5.13";
+  orientationQuirk = {
+    name = "Steam Deck orientation quirk";
+    patch = ../patches/kernel/5.13/0001-drm-Added-orientation-quirk-for-Valve-Steam-Deck.patch;
+  };
+in
 {
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_jovian;
+  options = {
+    jovian = {
+      hasKernelPatches = mkOption {
+        type = types.bool;
+        internal = true;
+        description = ''
+          Interface between modules to describe whether kernel patches are available
+        '';
+      };
+    };
+  };
+  config = mkMerge [
+    {
+      boot.kernelPackages = mkDefault pkgs.linuxPackages_jovian;
+      jovian.hasKernelPatches = hasKernelPatches;
+    }
+    (mkIf hasKernelPatches {
+      boot.kernelPatches = [
+        orientationQuirk
+      ];
+    })
+    (mkIf (!hasKernelPatches) {
+      warnings = [
+        ''
+          Kernel patches for improved default hardware support missing for kernel branch "${kernelBranch}".
+        ''
+      ];
+    })
+  ];
 }
