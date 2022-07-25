fb1514ca79395046a31d453d7c6cf7ef30c56dbe
Zhaofeng Li <hello@zhaofeng.li>
2022-07-25 16:04:20 -0700

    acp5x-ucm: Work around symlink detection bug in alsa-lib

--- pkgs/acp5x-ucm/default.nix
+++ pkgs/acp5x-ucm/default.nix
@@ -14,6 +14,12 @@ runCommandNoCC "acp5x-ucm-${version}" {
   mkdir -pv $out/share/alsa/ucm2/conf.d/acp5x
   cp -rv ${./acp5x}/* $out/share/alsa/ucm2/conf.d/acp5x/
 
+  # Temporary workaround for https://github.com/alsa-project/alsa-lib/pull/249
+  # Fixed in alsa-lib 1.2.7.2
+  mv $out/share/alsa/ucm2/conf.d/acp5x/acp5x.conf{,.real}
+  # not using the absolute path as the target is intentional - it's otherwise broken :/
+  ln -s acp5x.conf.real $out/share/alsa/ucm2/conf.d/acp5x/acp5x.conf
+
   # This *overrides the alsa-lib ucm.conf* if also linked in `pathsToLink`.
   cat > $out/share/alsa/ucm2/ucm.conf <<EOF
   Syntax 3
