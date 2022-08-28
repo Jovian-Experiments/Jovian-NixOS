eb4adaf7cce1f0f1174e3375c097bed2027c8464
Zhaofeng Li <hello@zhaofeng.li>
2022-08-28 00:30:19 -0600

    steam: Use steam-session
    This is much closer to the vanilla Steam Deck experience.

--- modules/steam.nix
+++ modules/steam.nix
@@ -8,26 +8,6 @@ let
     mkOption
     types
   ;
-  # ¯\_(ツ)_/¯
-  token = "steampal_stable_9a24a2bf68596b860cb6710d9ea307a76c29a04d";
-
-  # This script is used to hijack the call to `dbus-send` that the steam client
-  # will to to return to the desktop.
-  dbus-hijacker = (pkgs.writeShellScriptBin "dbus-send" ''
-    if echo "$@" | grep 'DisplayManager.*SwitchToUser.*doorstop.*plasma'; then
-      systemctl --user stop steam.target
-      exit 0
-    fi
-
-    # Snitches on unhandled calls to `dbus-send`
-    # echo dbus-send "$@" >> ~/dbus-hijacker.log
-
-    exec ${pkgs.dbus}/bin/dbus-send "$@"
-  '').overrideAttrs({ meta, ... }: { meta = meta // { piority = 9999; }; });
-
-  steam = pkgs.steam.override {
-    extraPkgs = pkgs: [ dbus-hijacker ];
-  };
 in
 {
   options = {
@@ -37,12 +17,10 @@ in
           type = types.bool;
           default = false;
           description = ''
-            Enables stopped-by-default systemd service to launch steam
-            in "PAL" user interface (steam deck interface).
+            Whether to enable the Steam Deck UI.
 
-            Use `systemctl --user start steam.target` to launch.
-
-            Use `systemctl --user stop steam.target` to stop.
+            When enabled, you can either launch the Steam Deck UI
+            from your Display Manager or by running `steam-session`.
           '';
         };
       };
@@ -53,49 +31,14 @@ in
       hardware.opengl.driSupport32Bit = true;
       hardware.pulseaudio.support32Bit = true;
 
-      systemd.user.services."steam" = {
-        enable = true;
-        serviceConfig = {
-          Restart = "always";
-          ExecStart = pkgs.writeShellScript "steam-pal-ui" ''
-            export PATH="${lib.makeBinPath ([
-              pkgs.gamescope
-              steam
-            ])}:$PATH"
-            # This forces the user in the steampal beta.
-            # Not ideal, as it's not reversed by disabling this service.
-            echo "${token}" > ~/.local/share/Steam/package/beta
-            exec gamescope \
-              --fullscreen \
-              --steam \
-              -- steam -steamos -gamepadui -pipewire-dmabuf
-          '';
-        };
-        unitConfig = {
-          ConditionPathExists = "/run/user/%U";
-        };
-        requisite = [ "steam.target" ];
-        bindsTo = [ "steam.target" ];
-      };
-      # This target is used to manage the "dependency" on running steam.
-      # Using `Upholds` here means the steam service will actually be
-      # restarted if it has been killed, or exited otherwise.
-      # Without this, `pkill gamescope` would not restart the service
-      # even though it declared `Restart=always`.
-      # ¯\_(ツ)_/¯
-      # Though, in a way it makes more sense to have a *target* here
-      # than a service.
-      systemd.user.targets."steam" = {
-        unitConfig = {
-          Upholds = [ "steam.service" ];
-        };
-        enable = true;
-        # wantedBy is not used, as this would make it a hard
-        # requirement on the graphical session.
-        # This service is only used to manage (start/stop) steam.
-        requisite = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-      };
+      environment.systemPackages = [ pkgs.steam-session ];
+
+      services.xserver.displayManager.sessionPackages = [ pkgs.steam-session ];
+
+      # Conflicts with power-button-handler
+      services.logind.extraConfig = ''
+        HandlePowerKey=ignore
+      '';
     })
   ];
 }
