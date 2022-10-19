0b7f63ab3ec1ea78ae46cfeeae2c4e2fbc3ae381
Zhaofeng Li <hello@zhaofeng.li>
2022-10-18 21:29:08 -0600

    nixpkgs.nix: Update pin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,8 +1,8 @@
 # Arbitrary known-good revision for default use.
 let
-  revision = "1ec61dd4167f04be8d05c45780818826132eea0d";
+  revision = "104e8082de1b20f9d0e1f05b1028795ed0e0e4bc";
 in
 import (builtins.fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/${revision}.tar.gz";
-  sha256 = "sha256:0aglyrxqkfwm4wxlz642vcgn0m350jv4nhhyq91cxylvs1avps54";
+  sha256 = "sha256:1y7j4bgk6wcipy9vmfmdgy8pv1wp3mq76sdjc4yib7xdn0bvgxvh";
 })
