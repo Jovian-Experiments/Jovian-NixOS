801e873be40c612122edf9c770a07e8f99d7e429
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-10-29 19:58:06 -0400

    jupiter-dock-updater-bin: 20220921.01 -> 20221026.01

--- pkgs/jupiter-dock-updater-bin/default.nix
+++ pkgs/jupiter-dock-updater-bin/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jupiter-dock-updater-bin";
-  version = "20220921.01";
+  version = "20221026.01";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "jupiter-dock-updater-bin";
     rev = "jupiter-${version}";
-    hash = "sha256-rlHUHIaRBHK5KlCklPh0X0Is6D2sVhB6h6yMZZDRPUk=";
+    hash = "sha256-Iu9oAy9wVIMowD+wOABIbLjA0Vgr7xndlz0/jhuDuVg=";
   };
 
   buildInputs = [
