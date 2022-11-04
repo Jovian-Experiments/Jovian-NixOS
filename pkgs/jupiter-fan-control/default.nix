7710a549c09e7ab7d2ef0add5b12fbbbb545d8d4
Zhaofeng Li <hello@zhaofeng.li>
2022-11-04 02:53:20 -0600

    jupiter-fan-control: 20220705.1 -> 20221031.1

--- pkgs/jupiter-fan-control/default.nix
+++ pkgs/jupiter-fan-control/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "jupiter-fan-control";
-  version = "20220705.1";
+  version = "20221031.1";
 
   # TODO: Replace with https://gitlab.steamos.cloud/jupiter/jupiter-fan-control
   # once it becomes public
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     owner = "Jovian-Experiments";
     repo = "jupiter-fan-control";
     rev = version;
-    sha256 = "sha256-Ij4yflupmlatbMFhGLUNv0bHo9H4tRlk2IX6pwnRFMk=";
+    sha256 = "sha256-5+6uHt1ykUtB8cJj6T5h0L9N2lPSkWsZS6U2+iO/WYc=";
   };
 
   buildInputs = [
