ad6800dbb3794d858bcb1140b43bb061e312eb77
Zhaofeng Li <hello@zhaofeng.li>
2022-11-05 13:38:27 -0600

    steamdeck/controller: Use uaccess to grant access to unprivileged users
    See vendor `60-steam-input.rules` in the upstream Steam package
    (steamPackages.steam).

--- modules/steamdeck/controller.nix
+++ modules/steamdeck/controller.nix
@@ -30,13 +30,13 @@ in
       # Necessary for the controller parts to work correctly.
       services.udev.extraRules = ''
         # This rule is necessary for gamepad emulation.
-        KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
+        KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
 
         # This rule is needed for basic functionality of the controller in Steam and keyboard/mouse emulation
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0660", TAG+="uaccess"
 
         # Valve HID devices over USB hidraw
-        KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+        KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0660", TAG+="uaccess"
       '';
     })
   ];
