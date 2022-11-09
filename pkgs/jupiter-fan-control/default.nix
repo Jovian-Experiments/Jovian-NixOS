e72558f568d06c07b77127178e2e2d09eeb4227d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-09 01:03:41 -0500

    jupiter-fan-control: 20221031.1 -> 20221107.1

--- pkgs/jupiter-fan-control/default.nix
+++ pkgs/jupiter-fan-control/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "jupiter-fan-control";
-  version = "20221031.1";
+  version = "20221107.1";
 
   # TODO: Replace with https://gitlab.steamos.cloud/jupiter/jupiter-fan-control
   # once it becomes public
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     owner = "Jovian-Experiments";
     repo = "jupiter-fan-control";
     rev = version;
-    sha256 = "sha256-5+6uHt1ykUtB8cJj6T5h0L9N2lPSkWsZS6U2+iO/WYc=";
+    sha256 = "sha256-i/wSbFRGAhlrFKCKmFRpgljkqZ3m9TkfWVS4HNGwVbU=";
   };
 
   buildInputs = [
