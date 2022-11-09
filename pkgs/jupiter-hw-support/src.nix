fedd124934e57a20d972cb526a8daf6717b02725
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-09 01:30:21 -0500

    jupiter-hw-support: 20221102.1 -> 20221108.1

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
 let
-  version = "20221102.1";
+  version = "20221108.1";
 in (fetchFromGitHub {
   name = "jupiter-hw-support-${version}";
   owner = "Jovian-Experiments";
   repo = "jupiter-hw-support";
   rev = "jupiter-${version}";
-  sha256 = "sha256-9wBY8KYHzj+WSS5fhIQEtZrNMaA+XJXWIsL6J9+NXL4=";
+  sha256 = "sha256-qb/YOWsBm213ChTM05XCehtX2zhYvXVxOgUMfdJpE9E=";
 }) // {
   inherit version;
 }
