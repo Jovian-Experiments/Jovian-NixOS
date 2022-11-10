0a06eedd429ba251b0e7b179adbf6f4ae62e8eda
Zhaofeng Li <hello@zhaofeng.li>
2022-11-10 02:18:26 -0700

    steamdeck/sound: Use environment.pathsToLink for UCM
    It's working now as of alsa-lib 1.2.7.2.

--- modules/steamdeck/sound.nix
+++ modules/steamdeck/sound.nix
@@ -23,11 +23,11 @@ in
         alsa.enable = lib.mkDefault true;
       };
 
-      # TODO: overlay on top of the vanilla alsa-lib using environment.pathsToLink
-      # Currently this does not work due to some weird path concatenation behavior in alsa-lib:
-      #
-      #     openat(AT_FDCWD, "/run/current-system/sw/share/alsa/ucm2/conf.d/acp5x//nix/store/hxm24krvwjyys9zfirn203sf3ncm44gs-acp5x-ucm-jupiter-20220310.1000/share/alsa/ucm2/conf.d/acp5x/HiFi.conf", O_RDONLY) = -1 ENOENT (No such file or directory)
-      environment.variables.ALSA_CONFIG_UCM2 = "${pkgs.jupiter-hw-support}/share/alsa/ucm2";
+      environment = {
+        pathsToLink = [ "share/alsa/ucm2" ];
+        variables.ALSA_CONFIG_UCM2 = "/run/current-system/sw/share/alsa/ucm2";
+        systemPackages = [ pkgs.jupiter-hw-support ];
+      };
     }
 
     # Pulseaudio
