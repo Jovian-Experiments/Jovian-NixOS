638d54302594e9087387d8dfb33c7c7d56d8841f
Zhaofeng Li <hello@zhaofeng.li>
2022-12-14 23:25:48 -0700

    jupiter-hw-support: 20221121.1 -> 20221213.1

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -1,13 +1,13 @@
 { fetchFromGitHub }:
 
 let
-  version = "20221121.1";
+  version = "20221213.1";
 in (fetchFromGitHub {
   name = "jupiter-hw-support-${version}";
   owner = "Jovian-Experiments";
   repo = "jupiter-hw-support";
   rev = "jupiter-${version}";
-  sha256 = "sha256-gFa0NgKQJSliaG57MF6im9Z27CXCjXu/zVQWyRan58A=";
+  sha256 = "sha256-0cHS2RU3qYmE2vfRm280jAU82c+OoZ4V5LBoSbqYXu0=";
 }) // {
   inherit version;
 }
