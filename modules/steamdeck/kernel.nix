57cafce9b5168d2872a7b0b7bf81f44a08dbcbbf
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-02-08 16:19:58 -0500

    steamdeck/kernel: Drop custom patches...
    ... all our patches have their equivalent in the current version.

--- modules/steamdeck/kernel.nix
+++ modules/steamdeck/kernel.nix
@@ -10,16 +10,6 @@ let
   ;
 
   cfg = config.jovian.devices.steamdeck;
-
-  kernelBranch = config.boot.kernelPackages.kernel.meta.branch;
-
-  # FIXME: Stop hardcoding patch to 5.13
-  # TODO: Make an attrset of kernel major versions with the appropriate patches.
-  hasKernelPatches = kernelBranch == "5.13";
-  orientationQuirk = {
-    name = "Steam Deck orientation quirk";
-    patch = ../../patches/kernel/5.13/0001-drm-Added-orientation-quirk-for-Valve-Steam-Deck.patch;
-  };
 in
 {
   options = {
@@ -31,32 +21,11 @@ in
           Whether to apply kernel patches if available.
         '';
       };
-      hasKernelPatches = mkOption {
-        type = types.bool;
-        internal = true;
-        default = false;
-        description = ''
-          Interface between modules to describe whether kernel patches are available
-        '';
-      };
     };
   };
   config = mkIf (cfg.enableKernelPatches) (mkMerge [
     {
       boot.kernelPackages = mkDefault pkgs.linuxPackages_jovian;
-      jovian.devices.steamdeck.hasKernelPatches = hasKernelPatches;
     }
-    (mkIf (cfg.hasKernelPatches) {
-      boot.kernelPatches = [
-        orientationQuirk
-      ];
-    })
-    (mkIf (!cfg.hasKernelPatches) {
-      warnings = [
-        ''
-          Kernel patches for improved default hardware support missing for kernel branch "${kernelBranch}".
-        ''
-      ];
-    })
   ]);
 }
