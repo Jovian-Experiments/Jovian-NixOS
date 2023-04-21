50140c9226a5aefda77f0e101ffbd624e32bf2ca
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-04-20 21:48:21 -0400

    linux-firmware: jupiter-20230110 -> jupiter-20230420

--- pkgs/linux-firmware/default.nix
+++ pkgs/linux-firmware/default.nix
@@ -4,9 +4,9 @@ linux-firmware.overrideAttrs(_: {
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "linux-firmware";
-    rev = "jupiter-20230110";
-    hash = "sha256-XHdqeWYYlobqCr53wc9GvBiMpzzOgEP785R5YhoCIwI=";
+    rev = "jupiter-20230420";
+    hash = "sha256-ys/7G+JsiuKQo9aL5MZjs4NxqDjK2bdJkLRJaoNeIDM=";
   };
 
-  outputHash = "sha256-v66fhA2WdYgrOLxEC6IHA6wDv9T5vLJV81DJq/sbMA0=";
+  outputHash = "sha256-eEeBS95gI7G9KVpc9boqRAdecrPc0EsfFD2nhh63fCY=";
 })
