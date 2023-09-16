c3f0b289298b3e390d95ebb3663bfb11f1c274e3
K900 <me@0upti.me>
2023-09-16 19:46:19 +0300

    mangohud: 0.6.9.1.r16 -> 0.6.9.1.r22

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, ... }@args:
 
 (callPackage ./upstream (removeAttrs args ["callPackage"])).overrideAttrs (old: {
-  version = "unstable-2023-04-23";
+  version = "unstable-2023-04-25";
   src = fetchFromGitHub {
     owner = "flightlessmango";
     repo = "MangoHud";
 
-    # As shipped in SteamOS 3 repo 0.6.9.1.r16.g1093de8
-    rev = "1093de8c4406a96642e139243911a571874c3d11";
-    hash = "sha256-D4E3C8+jcbEqoQ1Po8Bh5H3k0Fu36DttIaSjUjaRadc=";
+    # As shipped in SteamOS 3 repo 0.6.9.1.r22.g1d8f9f6
+    rev = "1d8f9f660135f460f4109e98d8725a75c908246a";
+    hash = "sha256-vyqMbdrc5s3vS5apFfzz0rI/y3bVlU8n/BXi4i+UkLU=";
   };
 })
