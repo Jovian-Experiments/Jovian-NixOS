c43a1a2772ff3983363775424845c37eb8b88b49
K900 <me@0upti.me>
2023-09-23 12:08:33 +0300

    jupiter-hw-support: resholve, add patch to use our protocol handler

--- pkgs/jupiter-hw-support/default.nix
+++ pkgs/jupiter-hw-support/default.nix
@@ -1,14 +1,66 @@
 { lib
 , stdenv
 , callPackage
-, python3
+, resholve
+, bash
+, coreutils
+, e2fsprogs
+, exfatprogs
+, f3
+, findutils
+, gawk
+, gnused
+, jq
+, parted
+, procps
+, systemd
+, util-linux
 }:
 
 let
   src = callPackage ./src.nix { };
-  pythonEnv = python3.withPackages (py: with py; [
-    evdev
-  ]);
+
+  solution = {
+    scripts = [ "bin/*" "lib/hwsupport/*.sh" ];
+    interpreter = "${bash}/bin/bash";
+    inputs = [
+      coreutils
+      e2fsprogs
+      exfatprogs
+      f3
+      findutils
+      gawk
+      gnused
+      jq
+      parted
+      procps
+      systemd
+      util-linux
+
+      "${placeholder "out"}/lib/hwsupport"
+    ];
+    execer = [
+      "cannot:${e2fsprogs}/bin/mkfs.ext4"
+      "cannot:${procps}/bin/pgrep"
+      "cannot:${systemd}/bin/systemctl"
+      "cannot:${systemd}/bin/udevadm"
+      "cannot:${util-linux}/bin/flock"
+
+      "cannot:${placeholder "out"}/lib/hwsupport/format-device.sh"
+    ];
+    fake = {
+      # we're using wrappers for these
+      external = [ "umount" ];
+    };
+    fix = {
+      "/usr/lib/hwsupport/format-device.sh" = true;
+    };
+    keep = {
+      # pre-applied via patch
+      # FIXME: why do we need to discard string context here?
+      "${builtins.unsafeDiscardStringContext "${systemd}/bin/systemd-run"}" = true;
+    };
+  };
 in
 stdenv.mkDerivation {
   pname = "jupiter-hw-support";
@@ -16,13 +68,14 @@ stdenv.mkDerivation {
   inherit src;
   inherit (src) version;
 
-  buildInputs = [ pythonEnv ];
-
   dontConfigure = true;
   dontBuild = true;
 
   installPhase = ''
     runHook preInstall
+
+    mkdir -p $out/bin
+    cp usr/bin/jupiter-check-support $out/bin
 
     mkdir -p $out/lib
     cp -r usr/lib/hwsupport $out/lib
@@ -45,6 +98,8 @@ stdenv.mkDerivation {
     # ALSA lib main.c:2573:(set_verb_user) error: failed to initialize new use case: HiFi
     # alsaucm: error failed to set _verb=HiFi: Exec format error
     sed -i 's|exec "echo|#exec "echo|g' $out/share/alsa/ucm2/conf.d/acp5x/HiFi*.conf
+
+    ${resholve.phraseSolution "jupiter-hw-support" solution}
 
     runHook postInstall
   '';
