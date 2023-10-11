443af9a085d6b2262b69302905a58bed9306f8fe
K900 <me@0upti.me>
2023-10-11 13:30:37 +0300

    jovian-steam-protocol-handler: replace with its own fhsenv
    Breaks dep cycle from steam -> polkit-helpers -> hwsupport -> handler -> steam

--- pkgs/jovian-steam-protocol-handler/default.nix
+++ pkgs/jovian-steam-protocol-handler/default.nix
@@ -1,9 +1,23 @@
 {
+  buildFHSEnv,
   writeShellScript,
-  steamPackages,
 }:
+# Just enough fhsenv to run just enough of Steam to actually send a URL to the _real_ Steam.
 # FIXME: this is a hack, replace with a better implementation
 # Investigate magic socket?
-writeShellScript "jovian-steam-protocol-handler" ''
-  exec ${steamPackages.steam-fhsenv.run}/bin/steam-run ~/.steam/root/ubuntu12_32/steam "$@"
-''
+buildFHSEnv {
+  name = "jovian-steam-protocol-handler";
+  
+  multiArch = true;
+  multiPkgs = pkgs: with pkgs; [
+    glibc
+    xorg.libX11
+    xorg.libxcb
+    xorg.libXau
+    xorg.libXdmcp
+  ];
+  
+  runScript = writeShellScript "jovian-steam-protocol-handler-impl" ''
+    exec ~/.steam/root/ubuntu12_32/steam "$@"
+  '';
+}
