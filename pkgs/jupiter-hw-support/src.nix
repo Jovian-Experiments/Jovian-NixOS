0dcd9a32bd223e5e4afca8544c4df0fad7f62034
K900 <me@0upti.me>
2023-10-27 09:48:32 +0300

    jupiter-hwsupport: 20231025.2 -> 20231026.1

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -7,13 +7,13 @@
 }:
 
 let
-  version = "20231025.2";
+  version = "20231026.1";
 in (applyPatches {
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "jupiter-hw-support";
     rev = "jupiter-${version}";
-    hash = "sha256-2984sTJKLQoTbBAwD1X3S+I3a2eGxdMC4Z1rt54D5NI=";
+    hash = "sha256-bnl193XmxrUrNw7SfLhZ7yhd3lF9TzEckgyJU06K964=";
   };
 
   patches = [
