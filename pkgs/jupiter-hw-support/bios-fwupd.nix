6c8316d8c66e81599e0c4543fc94ae6a27d8ffe3
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-11-14 23:07:15 -0500

    steamdeck-bios-fwupd: Rework to support galileo
    Support is not implemented, as we are currently lacking the GUID.

--- pkgs/jupiter-hw-support/bios-fwupd.nix
+++ pkgs/jupiter-hw-support/bios-fwupd.nix
@@ -13,12 +13,14 @@
 
 # The following parameters are for your enjoyment of flashing custom/older BIOSes:
 
-# The BIOS file (e.g., "F7A0110_sign.fd") or a directory containing it.
-, biosFile ? callPackage ./src.nix { }
+# Board prefixes to build the output for.
+, boards ? [
+  "F7A" # Jupiter
+  #"F7G" # Galileo
+]
 
-# The BIOS version (e.g., "0110").
-# If null, parsed from the BIOS file name.
-, biosVersion ? null
+# Directory containing the BIOS files.
+, biosSource ? callPackage ./src.nix { }
 
 # The release date of the BIOS for the fwupd manifest.
 , releaseDate ? "1970-01-01"
@@ -31,7 +33,7 @@ let
   version = if packageVersion != null then packageVersion else jupiter-hw-support.version;
 
 in runCommand "steamdeck-bios-fwupd-${version}" {
-  inherit biosFile biosVersion releaseDate;
+  inherit boards biosSource releaseDate;
 
   nativeBuildInputs = [ gcab ];
 
@@ -40,32 +42,49 @@ in runCommand "steamdeck-bios-fwupd-${version}" {
     license = licenses.unfreeRedistributableFirmware;
   };
 } ''
-  if [[ -d "$biosFile" ]]; then
-    biosFile=$(find "$biosFile" -regextype posix-extended -type f -regex ".*/F7A.*_sign.fd" | head -1)
-  fi
+  for board in $boards; do
+    >&2 echo ":: Building archive for board '$board'"
 
-  >&2 echo ":: Found BIOS file $biosFile"
+    biosFile=$(find "$biosSource" -regextype posix-extended -type f -regex ".*/''${board}[0-9]{4}_sign.fd" | head -1)
 
-  if [[ -z "$biosVersion" ]]; then
-    versionPair=$(basename "$biosFile" | sed 's/^.*F7A\([0-9]\{2\}\)\([0-9]\{2\}\).*$/\1 \2/; t; q1')
-  else
-    versionPair=$(echo "$biosVersion" | sed 's/^\([0-9]\{2\}\)\([0-9]\{2\}\)$/\1 \2/; t; q1')
-  fi
+    >&2 echo ":: Found BIOS file '$biosFile'"
 
-  get_numeric_ver() { printf "%d" "0x00$100$2"; }
-  export versionNumber=$(get_numeric_ver $versionPair)
+    versionPair=$(basename "$biosFile" | sed 's/^.*'"$board"'\([0-9]\{2\}\)\([0-9]\{2\}\).*$/\1 \2/; t; q1')
 
-  >&2 echo ":: Detected numeric BIOS version $versionNumber"
+    get_numeric_ver() { printf "%d" "0x00$100$2"; }
+    export versionNumber=$(get_numeric_ver $versionPair)
 
-  export filename=$(basename "$biosFile")
-  export sha1=$(sha1sum "$biosFile" | awk '{ print $1 }')
-  export sha256=$(sha1sum "$biosFile" | awk '{ print $1 }')
-  substituteAll ${./SteamDeckBIOS.metainfo.xml} SteamDeckBIOS.metainfo.xml
+    >&2 echo ":: Detected numeric BIOS version $versionNumber"
 
-  >&2 echo ":: Generated metainfo file"
-  cat -n SteamDeckBIOS.metainfo.xml
+    case "$board" in
+      F7A)
+        guid=bbb1cf06-f7b9-4466-adcc-6b8815bd99e6
+        model="LCD [Jupiter]"
+        ;;
+      #F7G)
+      #  guid=00000000-0000-0000-0000-000000000000
+      #  model="OLED [Galileo]"
+      #  ;;
+      *)
+        >&2 echo ":: No GUID for board '$board'"
+        exit 1
+        ;;
+    esac
 
-  mkdir -p $out
-  cp "$biosFile" .
-  gcab -c $out/bios.cab SteamDeckBIOS.metainfo.xml "$filename"
+    export model
+    export board
+    export guid
+    export filename=$(basename "$biosFile")
+    export sha1=$(sha1sum "$biosFile" | awk '{ print $1 }')
+    export sha256=$(sha1sum "$biosFile" | awk '{ print $1 }')
+    substituteAll ${./SteamDeckBIOS.metainfo.xml} SteamDeckBIOS.metainfo.xml
+
+    >&2 echo ":: Generated metainfo file"
+
+    archiveName=$(basename $biosFile | sed 's/_sign.*//')
+
+    mkdir -p $out
+    cp "$biosFile" .
+    gcab -c $out/"$archiveName".cab SteamDeckBIOS.metainfo.xml "$filename"
+  done
 ''
