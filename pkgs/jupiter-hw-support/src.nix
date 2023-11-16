34fd2b2d851d64d174a805a113aa22f8e8b5bfee
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-11-15 22:29:14 -0500

    jupiter-hw-support: 20231114.1 -> 20231115.1
    Trivial change:
    
     - https://github.com/Jovian-Experiments/jupiter-hw-support/compare/jupiter-20231114.1...jupiter-20231115.1

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -7,13 +7,13 @@
 }:
 
 let
-  version = "20231114.1";
+  version = "20231115.1";
 in (applyPatches {
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "jupiter-hw-support";
     rev = "jupiter-${version}";
-    hash = "sha256-IXEqWNTuzO9HVwHDG+2UI4ALXGcTk9joQb/GjkmsiH0=";
+    hash = "sha256-m36uJMdMpONXUMm33VcsoX+zMr/5MosU7ivq7wxogvw=";
   };
 
   patches = [
