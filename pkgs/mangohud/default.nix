39b14dad8487f7733a00b35561d6d15af5d76f04
K900 <me@0upti.me>
2023-12-01 13:10:58 +0300

    mangohud: 0.7.0.r68 -> 0.7.0.r76
    This is effectively identical to what Valve just shipped.

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -1,13 +1,13 @@
 { callPackage, fetchFromGitHub, git, ... }@args:
 
 (callPackage ./upstream (removeAttrs args ["callPackage" "git"])).overrideAttrs (old: {
-  version = "0.7.0.r68";
+  version = "0.7.0.r76";
   src = fetchFromGitHub {
     owner = "flightlessmango";
     repo = "MangoHud";
 
-    rev = "ea725ed1d2000d5409e701dc770282b28e80d5e6";
-    hash = "sha256-ZvpAQsM7KV3fQLxNBzWNCYVSjR0ILESIgZq9AigiqGg=";
+    rev = "4646e2e4f6fe3067740b9eb806679b7576231f10";
+    hash = "sha256-hl3B99J/h655uZTGCGkqU5bAFqSGoBskyq8khNoVoCA=";
   };
 
   nativeBuildInputs = old.nativeBuildInputs ++ [ git ];
