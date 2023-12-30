68a88f08ef8b06f80b3a9ae09ae9172f50141c3e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-12-30 18:46:04 -0500

    steamos/sysctl: Add missing vendor configuration

--- modules/steamos/sysctl.nix
+++ modules/steamos/sysctl.nix
@@ -9,6 +9,7 @@ let
     types
   ;
   cfg = config.jovian.steamos;
+  mkJovianDefault = lib.mkOverride 999;
 in
 {
   options = {
@@ -34,6 +35,16 @@ in
         "kernel.sched_wakeup_granularity_ns" = mkDefault 500000;
         "kernel.sched_migration_cost_ns" = mkDefault 50000;
         "kernel.sched_nr_migrate" = mkDefault 128;
+
+        # > This is required due to some games being unable to reuse their TCP ports
+        # > if they're killed and restarted quickly - the default timeout is too large.
+        #  - https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/4c7b67cc5553ef6c15d2540a08a737019fc3cdf1
+        "net.ipv4.tcp_fin_timeout" = mkDefault 5;
+        
+        # > USE MAX_INT - MAPCOUNT_ELF_CORE_MARGIN.
+        # > see comment in include/linux/mm.h in the kernel tree.
+        #  - https://github.com/Jovian-Experiments/steamos-customizations-jupiter/commit/e21954bb4743635a9c53016def5158469fa6d7a8
+        "vm.max_map_count" = mkJovianDefault 2147483642;
       };
     })
   ];
