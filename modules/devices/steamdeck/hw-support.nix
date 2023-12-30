04aabaf323b9c3561c8e5e6160387eedf21387b5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-12-30 18:42:51 -0500

    devices/steamdeck: Drop sysctl config

--- modules/devices/steamdeck/hw-support.nix
+++ modules/devices/steamdeck/hw-support.nix
@@ -15,14 +15,6 @@ in
 {
   options = {
     jovian.devices.steamdeck = {
-      enableDefaultSysctlConfig = mkOption {
-        default = cfg.enable;
-        defaultText = lib.literalExpression "config.jovian.devices.steamdeck.enable";
-        type = types.bool;
-        description = ''
-          Whether to enable stock sysctl configs.
-        '';
-      };
       enableDefaultCmdlineConfig = mkOption {
         default = cfg.enable;
         defaultText = lib.literalExpression "config.jovian.devices.steamdeck.enable";
@@ -97,17 +89,6 @@ in
         #  - https://github.com/Jovian-Experiments/jupiter-hw-support/blob/jupiter-20231212.1/etc/default/grub-steamos
         "spi_amd.speed_dev=1"
       ];
-    })
-    (mkIf (cfg.enableDefaultSysctlConfig) {
-      boot.kernel.sysctl = {
-        # From 99-sysctl.conf
-        "kernel.sched_cfs_bandwidth_slice_u" = mkDefault 3000;
-        "kernel.sched_latency_ns" = mkDefault 3000000;
-        "kernel.sched_min_granularity_ns" = mkDefault 300000;
-        "kernel.sched_wakeup_granularity_ns" = mkDefault 500000;
-        "kernel.sched_migration_cost_ns" = mkDefault 50000;
-        "kernel.sched_nr_migrate" = mkDefault 128;
-      };
     })
   ];
 }
