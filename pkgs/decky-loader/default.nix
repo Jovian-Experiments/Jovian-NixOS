a2886427acd0b5abf652f9e61c99e91deaaaf40f
K900 <me@0upti.me>
2024-02-03 15:02:21 +0300

    decky-loader: 2.10.14 -> 2.10.15-pre1

--- pkgs/decky-loader/default.nix
+++ pkgs/decky-loader/default.nix
@@ -7,9 +7,9 @@
 , python3
 }:
 let
-  version = "2.10.14";
-  hash = "sha256-ydD5hcTNLsBteipdiJGWqK9TGqE1uvP2pMp+HfBUkxk=";
-  npmHash = "sha256-UYfZDgDjgYTqe/IxAet+NkjfaeYhJIoCg368ykaMxSE=";
+  version = "2.10.15-pre1";
+  hash = "sha256-ChzGrTqrChxZoMqPyvlaPXDzNhcxoTLgCXp7ehZAKsI=";
+  npmHash = "sha256-uXQ4jUdkvexD3l1I0nBDr1Xa5o9+CCOzMYD0gAQxnGA=";
 
   src = fetchFromGitHub {
     owner = "SteamDeckHomebrew";
