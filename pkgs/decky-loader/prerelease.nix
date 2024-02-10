1694004200154ecbf645d18e1af217d8e273beae
K900 <me@0upti.me>
2024-02-10 15:17:43 +0300

    decky-loader-prerelease: 2.10.15-pre1 -> 2.10.15-pre2

--- pkgs/decky-loader/prerelease.nix
+++ pkgs/decky-loader/prerelease.nix
@@ -1,5 +1,5 @@
 import ./generic.nix {
-  version = "2.10.15-pre1";
-  hash = "sha256-ChzGrTqrChxZoMqPyvlaPXDzNhcxoTLgCXp7ehZAKsI=";
+  version = "2.10.15-pre2";
+  hash = "sha256-19XzjM4yRGh39OrfyCJef9JTgWhMuF8Pt+SX620Im/k=";
   npmHash = "sha256-uXQ4jUdkvexD3l1I0nBDr1Xa5o9+CCOzMYD0gAQxnGA=";
 }
