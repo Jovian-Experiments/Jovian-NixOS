fad26bed100a48bdddc2e11e173402b2bad7b9e8
K900 <me@0upti.me>
2024-02-15 17:23:57 +0300

    decky-loader-prerelease: 2.11.0-pre3 -> 2.11.1-pre1

--- pkgs/decky-loader/prerelease.nix
+++ pkgs/decky-loader/prerelease.nix
@@ -1,5 +1,5 @@
 import ./generic.nix {
-  version = "2.11.0-pre3";
-  hash = "sha256-o9zDhJTF4/x2HpdEzIdn71mA4c/QRKYx8lxvWHdCvHc=";
+  version = "2.11.1-pre1";
+  hash = "sha256-Jf4P7w43MlDo+o7kNZYagMn0UbJfhp4L7Xwjbep9AUQ=";
   npmHash = "sha256-GJ+isvwi0wfMfQusH5F3mUA9hEdZO9Pvw9Qm5+Iv34w=";
 }
