a8e81740e8dd3a2a1b68b9573c024fe2a5cc6a55
K900 <me@0upti.me>
2024-04-07 21:43:33 +0300

    modules/steam: add more steam

--- modules/steam/steam.nix
+++ modules/steam/steam.nix
@@ -32,6 +32,9 @@ in
       };
     }
     {
+      # Enable the usual desktop Steam stuff
+      programs.steam.enable = mkDefault true;
+
       # Enable MTU probing, as vendor does
       # See: https://github.com/ValveSoftware/SteamOS/issues/1006
       # See also: https://www.reddit.com/r/SteamDeck/comments/ymqvbz/ubisoft_connect_connection_lost_stuck/j36kk4w/?context=3
