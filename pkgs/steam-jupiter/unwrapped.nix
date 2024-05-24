d2e7daaa6763b894e92afe8e79237f162b0b7954
K900 <me@0upti.me>
2024-05-24 11:00:14 +0300

    steam-jupiter: 1.0.0.78-1.2 -> 1.0.0.79-1.1

--- pkgs/steam-jupiter/unwrapped.nix
+++ pkgs/steam-jupiter/unwrapped.nix
@@ -6,10 +6,10 @@
 { steam-original, fetchurl }:
 
 let
-  version = "1.0.0.78-1.2";
+  version = "1.0.0.79-1.1";
   bundle = fetchurl {
     url = "https://steamdeck-packages.steamos.cloud/archlinux-mirror/sources/jupiter-main/steam-jupiter-stable-${version}.src.tar.gz";
-    hash = "sha256-mgCdDDPyIs/LbqUcbiN/sDCPFSE2XXJIFIZBVgwl358=";
+    hash = "sha256-izCFnX4Qk2M+jNtT3urGQoNbVGEN3v0SU3LfTWqS3ho=";
   };
 
 in steam-original.overrideAttrs (old: {
