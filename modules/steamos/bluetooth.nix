b572dce86304d81449336875fdc7ddb4e5049f3d
K900 <me@0upti.me>
2024-07-03 07:23:59 +0300

    bluetooth: sync config with vendor bluez 5.76-2.2

--- modules/steamos/bluetooth.nix
+++ modules/steamos/bluetooth.nix
@@ -29,7 +29,7 @@ in
         MultiProfile = "multiple";
         FastConnectable = true;
         # enable experimental LL privacy, experimental offload codecs
-        KernelExperimental = "15c0a148-c273-11ea-b3de-0242ac130004,a6695ace-ee7f-4fb9-881a-5fac66c629af";
+        KernelExperimental = "15c0a148-c273-11ea-b3de-0242ac130004";
       };
       LE = {
         ScanIntervalSuspend = 2240;
