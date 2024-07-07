377912dbe4f0dbdd5d96ff12fbd30aa64c5fdc73
hacker1024 <hacker1024@users.sourceforge.net>
2024-07-07 18:03:38 +1000

    deckbd: 0-unstable-2023-03-16 -> 0-unstable-2023-07-01

--- pkgs/deckbd/default.nix
+++ pkgs/deckbd/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation {
   pname = "deckbd";
-  version = "0-unstable-2023-03-16";
+  version = "0-unstable-2024-07-01";
 
   src = fetchFromGitHub {
     owner = "Ninlives";
     repo = "deckbd";
-    rev = "327a8c91159e1b7faa2f12b5e11060b2eb9947a8";
-    hash = "sha256-T7iYl1xWtk39XMUUWm1pK0WVm5UK656HmqWHKDmJ220=";
+    rev = "1d2c71f2c096fbfa42624dd820a9d11a35c64826";
+    hash = "sha256-Svp/5Mo/XkiptbTM3E4QhSRxC6rMeF0t3eTq9BUjLbs=";
   };
 
   nativeBuildInputs = [ pkg-config ];
@@ -26,7 +26,7 @@ stdenv.mkDerivation {
   meta = {
     description = "Steam Deck controller keymapper";
     homepage = "https://github.com/Ninlives/deckbd";
-    license = lib.licenses.unfree; # https://github.com/Ninlives/deckbd/issues/1
+    license = lib.licenses.gpl3Only;
     mainProgram = "deckbd";
     platforms = lib.platforms.linux;
   };
