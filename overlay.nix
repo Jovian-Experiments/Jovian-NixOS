4fb7131c54638c07c2074516fce49ade33ecc5dc
K900 <me@0upti.me>
2024-07-10 20:33:52 +0300

    gamescope: 3.14.23 -> 3.14.24

--- overlay.nix
+++ overlay.nix
@@ -23,7 +23,7 @@ rec {
 
   gamescope = import ./pkgs/gamescope {
     gamescope' = super.gamescope;
-    inherit (final) fetchFromGitHub cmake;
+    inherit (final) fetchFromGitHub;
   };
   gamescope-wsi = gamescope.override {
     enableExecutable = false;
--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -1,23 +1,18 @@
 { gamescope'
 , fetchFromGitHub
-, cmake
 }:
 
 # NOTE: vendoring gamescope for the time being since we want to match the
 #       version shipped by the vendor, ensuring feature level is equivalent.
 
 gamescope'.overrideAttrs(old: rec {
-  version = "3.14.23";
+  version = "3.14.24";
 
   src = fetchFromGitHub {
     owner = "ValveSoftware";
     repo = "gamescope";
     rev = version;
     fetchSubmodules = true;
-    hash = "sha256-qXwCzNGlkGmO3BkQ74tJxufmjh4dUWzIgjHzDCEShU8=";
+    hash = "sha256-+8uojnfx8V8BiYAeUsOaXTXrlcST83z6Eld7qv1oboE=";
   };
-
-  # Force vendored OpenVR
-  buildInputs = builtins.filter (p: p.pname != "openvr") old.buildInputs;
-  nativeBuildInputs = old.nativeBuildInputs ++ [ cmake ];
 })
