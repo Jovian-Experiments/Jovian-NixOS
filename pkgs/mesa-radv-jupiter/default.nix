8e848b580d36b320f1499c976be8f56ca1c941f9
K900 <me@0upti.me>
2024-09-06 08:29:47 +0300

    mesa-radv-jupiter: 24.1.0-steamos-24.4.0 -> 24.1.0-steamos-24.4.1

--- pkgs/mesa-radv-jupiter/default.nix
+++ pkgs/mesa-radv-jupiter/default.nix
@@ -1,7 +1,7 @@
 { stdenv, mesa, fetchFromGitHub }:
 let
   version = "24.1.0";
-  jupiterVersion = "steamos-24.4.0";
+  jupiterVersion = "steamos-24.4.1";
 in stdenv.mkDerivation {
   pname = "mesa";
   version = "${version}.${jupiterVersion}";
@@ -10,7 +10,7 @@ in stdenv.mkDerivation {
     owner = "Jovian-Experiments";
     repo = "mesa";
     rev = jupiterVersion;
-    hash = "sha256-fWtEH8Ln1QZKMa7Y8sLexWyQYhNIuMhIUQPy198Oopg=";
+    hash = "sha256-f+gLumxWL7NmBq+dvwte7y88BPFjnvZfYDKxcxxKrs4=";
   };
 
   inherit (mesa) buildInputs nativeBuildInputs propagatedBuildInputs;
