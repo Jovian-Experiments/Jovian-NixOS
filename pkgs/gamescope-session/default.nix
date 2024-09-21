faf4c746c068dd8e41b1fa5f18beeabd34d4064c
K900 <me@0upti.me>
2024-09-21 10:06:30 +0300

    gamescope{,-session}: 3.15.9 -> 3.15.10

--- pkgs/gamescope-session/default.nix
+++ pkgs/gamescope-session/default.nix
@@ -105,13 +105,13 @@ let
   };
 in stdenv.mkDerivation(finalAttrs: {
   pname = "gamescope-session";
-  version = "3.15.9-1";
+  version = "3.15.10-1";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "PKGBUILDs-mirror";
     rev = "jupiter-main/gamescope-${finalAttrs.version}";
-    hash = "sha256-aPyhI7I3RyFjVgVpZLzUbbMP6zTjzWlqMXcx4fS2z7U=";
+    hash = "sha256-bf4surRXvUlrDd/mXF1cvBztwU1KTy8JzjW2yDOC9AA=";
   };
 
   patches = [
--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -1,21 +1,18 @@
 { gamescope'
 , fetchFromGitHub
-, writeShellScriptBin
 }:
 
 # NOTE: vendoring gamescope for the time being since we want to match the
 #       version shipped by the vendor, ensuring feature level is equivalent.
 
 gamescope'.overrideAttrs(old: rec {
-  version = "3.15.9";
+  version = "3.15.10";
 
   src = fetchFromGitHub {
     owner = "ValveSoftware";
     repo = "gamescope";
     rev = version;
     fetchSubmodules = true;
-    hash = "sha256-+BRinPyh8t9HboT0uXPEu+sSJz9qCZshlfzDfZDA41Q=";
+    hash = "sha256-7gljTLjW/tnCS2b77i/oWTOA2jCVj9GEV4FzfLqV9Uk=";
   };
-
-  nativeBuildInputs = old.nativeBuildInputs ++ [ (writeShellScriptBin "git" "echo ${version}") ];
 })
