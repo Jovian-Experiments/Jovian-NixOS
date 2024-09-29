d096ede4d5c61e9b88c1faa2e4fe51707eff44cd
hot burger <33610620+hotburger@users.noreply.github.com>
2024-09-28 19:33:25 -0700

    more descriptive steamos-automount patch name

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
       systemd = systemd;
       src = ./jovian.patch;
     })
-    ./btrfs.patch
+    ./steamos-automount-add-btrfs-and-f2fs-support.patch
     # Fix controller updates with python-hid >= 1.0.6
     ./hid-1.0.6.patch
   ];
