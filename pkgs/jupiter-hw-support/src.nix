81d4c94d55c243535f4c05881ccc7f79b8cfe7fa
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-09-30 02:22:16 -0400

    jupiter-hw-support: Two minor fixes for silly system configs

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -27,6 +27,9 @@ stdenv.mkDerivation rec {
     ./hid-1.0.6.patch
     # Remove `deck` username assumption
     ./0001-Jovian-Ensure-automounting-works-for-any-UID-1000-us.patch
+    # Minor fixes against silly environments
+    ./0001-steamos-automount-Harden-against-missing-run-media.patch
+    ./0001-format-device-Harden-against-mountpoint-being-listed.patch
   ];
 
   installPhase = ''
