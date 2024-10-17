bb69165ff372ddbd3228a03513922acd783040e8
K900 <me@0upti.me>
2024-10-17 18:07:10 +0300

    linux-firmware-jupiter: hack hack

--- pkgs/linux-firmware/default.nix
+++ pkgs/linux-firmware/default.nix
@@ -9,4 +9,8 @@ linux-firmware.overrideAttrs(_: rec {
     rev = "jupiter-${version}";
     hash = "sha256-mqBREldC1/nRzjDyPCneeMhDSW4w4VTH2K4F2IDYVxo=";
   };
+
+  # FIXME: workaround newer linux-firmware needing `install dedup`
+  # and this linux-firmware being too old to have `dedup`
+  installTargets = [ "install" ];
 })
