446559f3ae9886cf9792cda72da90125d2ec459c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-11-01 23:39:40 -0400

    jovian-greeter: Ensure exceptions get logged
    Without this, failure in the greeter would just crash. Not useful.

--- pkgs/jovian-greeter/greeter.py
+++ pkgs/jovian-greeter/greeter.py
@@ -185,6 +185,15 @@ class Context:
 
         return None
 
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logging.root.handlers = [
