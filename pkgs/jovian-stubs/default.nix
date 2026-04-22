edc5075974e5ecc3d530474ad074f459b6c34022
Simon Gardling <titaniumtown@proton.me>
2026-04-22 00:04:21 -0400

    jovian-stubs: add stub for steamos-mandatory-update
    Newer Steam clients have started to probe for "mandatory" updates.
    Needed a new stub for this new command.

--- pkgs/jovian-stubs/default.nix
+++ pkgs/jovian-stubs/default.nix
@@ -5,6 +5,7 @@ stdenv.mkDerivation {
   buildCommand = ''
     install -D -m 755 ${./steamos-factory-reset-config} $out/bin/steamos-factory-reset-config
     install -D -m 755 ${./steamos-firmware-update} $out/bin/steamos-firmware-update
+    install -D -m 755 ${./steamos-mandatory-update} $out/bin/steamos-mandatory-update
     install -D -m 755 ${./steamos-reboot} $out/bin/steamos-reboot
     install -D -m 755 ${./steamos-select-branch} $out/bin/steamos-select-branch
     install -D -m 755 ${./steamos-session-select} $out/bin/steamos-session-select
