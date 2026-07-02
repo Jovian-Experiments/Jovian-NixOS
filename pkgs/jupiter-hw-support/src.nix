1f3338e44b0780473da08de29fb37f8f85adf2aa
K900 <me@0upti.me>
2026-07-02 18:07:09 +0300

    jupiter-hw-support: 20260615.1 -> 20260630.2

--- pkgs/jupiter-hw-support/src.nix
+++ pkgs/jupiter-hw-support/src.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jupiter-hw-support-source";
-  version = "20260615.1";
+  version = "20260630.2";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "jupiter-hw-support";
     rev = "jupiter-${version}";
-    hash = "sha256-Udjs/6iCRzp5m/IeOXQOKL/Z6D/wfkcudA9cxogbxXI=";
+    hash = "sha256-ZYtm1H65zHF6iY6U7VVG9jeRz1LPugKTtw44TW2YDz8=";
   };
 
   patches = [
