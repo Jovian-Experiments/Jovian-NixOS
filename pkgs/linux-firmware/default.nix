69b458491084c117e6db8761aa52597e67446296
K900 <me@0upti.me>
2026-07-15 14:42:21 +0300

    linux-firmware-jupiter: 20260610.1 -> 20260712.1

--- pkgs/linux-firmware/default.nix
+++ pkgs/linux-firmware/default.nix
@@ -1,13 +1,13 @@
 { linux-firmware, fetchFromGitHub }:
 
 linux-firmware.overrideAttrs(_: rec {
-  version = "20260610.1";
+  version = "20260712.1";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "linux-firmware";
     rev = "jupiter-${version}";
-    hash = "sha256-PbNUw9tLQLrWNFb9Pr5ajXviqZm5X6E8NCLogFulYY4=";
+    hash = "sha256-UDtxeWCknIdnwPBTso3VFJFnpURGtPQnfxZjxA4n8Vk=";
   };
 
   # clobber nixpkgs patches
