463802032a04f99581cb3f9b111313e5c555892e
K900 <me@0upti.me>
2026-08-13 11:37:36 +0300

    pipewire-jupiter: 1.6.8-1.2 -> 1.6.8-1.3

--- pkgs/pipewire/default.nix
+++ pkgs/pipewire/default.nix
@@ -1,11 +1,11 @@
 { pipewire', fetchFromGitHub }:
 pipewire'.overrideAttrs (_: {
-  version = "1.6.8-1.2"; # 1.2 is a rebuild, no source changes
+  version = "1.6.8-1.3";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "pipewire";
-    rev = "1.6.8-jupiter1.1";
-    hash = "sha256-6A5LldWaxMHO9D4TnoPhzvZRMZEEPcrrVNAYDf5/hGE=";
+    rev = "1.6.8-jupiter1.3";
+    hash = "sha256-uPxuqcerXxPMIL6A/rQIwUuJD1aSSE0MKBojLt4LoHY=";
   };
 })
