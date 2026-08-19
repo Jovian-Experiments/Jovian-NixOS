d31be35dadbe457cbe8fec4f0efae6dbf723835d
K900 <me@0upti.me>
2026-08-19 15:39:27 +0300

    gamescope-session: 3.16.25-3 -> 3.16.25-4

--- pkgs/gamescope-session/default.nix
+++ pkgs/gamescope-session/default.nix
@@ -112,13 +112,13 @@ let
   };
 in stdenv.mkDerivation(finalAttrs: {
   pname = "gamescope-session";
-  version = "3.16.25-3";
+  version = "3.16.25-4";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "PKGBUILDs-mirror";
     rev = "jupiter-main/gamescope-${finalAttrs.version}";
-    hash = "sha256-hYJ1aL4T+zSm+iFCoXXrwcgEPTj3VrtYy5VQ2vXAxJc=";
+    hash = "sha256-EXrmdFATYnKrMD3+ipZjEpgjLTcYUgCVIceoOiADiHE=";
   };
 
   patches = [
