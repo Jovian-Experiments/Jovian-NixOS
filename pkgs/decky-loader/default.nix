6087aab8ab19bc314bbe132c6f3114a85d772375
K900 <me@0upti.me>
2026-09-10 10:37:40 +0300

    decky-loader: 3.2.6 -> 3.2.8

--- pkgs/decky-loader/default.nix
+++ pkgs/decky-loader/default.nix
@@ -10,13 +10,13 @@
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "decky-loader";
-  version = "3.2.6";
+  version = "3.2.8";
 
   src = fetchFromGitHub {
     owner = "SteamDeckHomebrew";
     repo = "decky-loader";
     rev = "v${version}";
-    hash = "sha256-p1bkLsZedTZ29POqdaXvVpPXzg9kBTKgUxkkEAyAkT0=";
+    hash = "sha256-Y2dMTKLXtZAyXuWhnS/jbqjCYyWvSChslt/YxIBbWXw=";
   };
 
   # confuses our pnpm tooling
--- pkgs/decky-loader/prerelease.nix
+++ pkgs/decky-loader/prerelease.nix
@@ -1,17 +1,4 @@
 {
   decky-loader,
 }:
-decky-loader.overridePythonAttrs(old: rec {
-  pname = "decky-loader";
-  version = "3.2.8-pre1";
-
-  src = old.src.override {
-    rev = "v${version}";
-    hash = "sha256-zqHqg9EuWXss+4yNVtIRCv1oq6/hPlhRB1oe9q7xLEc=";
-  };
-
-  pnpmDeps = old.pnpmDeps.override {
-    inherit version src;
-    hash = "sha256-OHimg85kcjk+Tq1Yv8TA9CfPDVzxdgPpzTi2mxyPs4s=";
-  };
-})
+decky-loader
