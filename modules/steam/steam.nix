aedbb7dfcfdb4ad4a09170ba4de428face154ede
foxtrottt <code@olympus.moe>
2026-09-11 00:39:38 -0400

    steam: Added udev rules related to fremont

--- modules/steam/steam.nix
+++ modules/steam/steam.nix
@@ -141,6 +141,30 @@ in
 
         # Steam Controller udev write access
         KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
+
+        # Steam Controller Wakeup Support
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1102", ATTR{power/wakeup}="enabled"
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1142", ATTR{power/wakeup}="enabled"
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1302", ATTR{power/wakeup}="enabled"
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1304", ATTR{power/wakeup}="enabled"
+        ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="1305", ATTR{power/wakeup}="enabled"
+
+
+        # LED write access
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user}'/sys/class/leds/%k/brightness'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/brightness_scale'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/brightness_startup'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/effect'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/enabled'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/mode'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/multi_intensity'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/multi_intensity_startup'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/profile'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/speed'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/delay'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/trigger'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/delay_on'"
+        SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chown ${cfg.user} '/sys/class/leds/%k/delay_off'"
       '';
 
       # The responsibility for the equivalent action when out of battery charge is
