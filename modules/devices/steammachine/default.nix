b9c192ba5e0baf6743ce7c505ebdb6cac1a441e5
foxtrottt <code@olympus.moe>
2026-09-10 22:46:17 -0400

    devices/steammachine: Init support for Steam Machine (fremont)

---
+++ modules/devices/steammachine/default.nix
@@ -0,0 +1,40 @@
+# Steam Machine-specific configurations
+#
+# jovian.devices.steammachine
+
+{ config, lib, ... }:
+
+let
+  inherit (lib)
+    mkIf
+    mkOption
+    types
+  ;
+  cfg = config.jovian.devices.steammachine;
+in
+{
+  imports = [
+    ./kernel.nix
+    ./firmware.nix
+    ./controller.nix
+    ./perf-control.nix
+    ./sound.nix
+  ];
+
+  options = {
+    jovian.devices.steammachine = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable Steam Machine-specific configurations.
+        '';
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    jovian.hardware.has = {
+      amd.gpu = true;
+    };
+  };
+}
