71c7c54cf8c69583cdaab23cf121573a9f5f5d74
K900 <me@0upti.me>
2026-09-15 08:05:20 +0300

    decky-loader: 3.2.8 -> 3.2.9

--- pkgs/decky-loader/default.nix
+++ pkgs/decky-loader/default.nix
@@ -10,32 +10,21 @@
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "decky-loader";
-  version = "3.2.8";
+  version = "3.2.9";
 
   src = fetchFromGitHub {
     owner = "SteamDeckHomebrew";
     repo = "decky-loader";
     rev = "v${version}";
-    hash = "sha256-Y2dMTKLXtZAyXuWhnS/jbqjCYyWvSChslt/YxIBbWXw=";
+    hash = "sha256-XhW+bbsEhWnD/1c3QVHAQz6AAo824b/hbZ1t/VZE1po=";
   };
-
-  # confuses our pnpm tooling
-  postPatch = ''
-    rm frontend/pnpm-workspace.yaml
-  '';
 
   pnpmDeps = fetchPnpmDeps {
     fetcherVersion = 4;
     inherit pname version src;
-
-    # copy here because of sourceRoot
-    postPatch = ''
-      rm pnpm-workspace.yaml
-    '';
-
     pnpm = pnpm_11;
     sourceRoot = "${src.name}/frontend";
-    hash = "sha256-OHimg85kcjk+Tq1Yv8TA9CfPDVzxdgPpzTi2mxyPs4s=";
+    hash = "sha256-w4UFsNqy8fYjpQ5jgPRQ4bfVZJb3aitYUsnf4PP8Itc=";
   };
 
   pyproject = true;
